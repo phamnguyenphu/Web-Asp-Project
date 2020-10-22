2daa263254ff4b0361404d0daee9815e4517fa5f
phamnguyenphu <phamnguyenphucp9@gmail.com>
2020-10-22 22:42:27 +0700

    Sửa đổi UI

---
+++ WebAspProject/WebAspProject/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAspProject.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult shopCategory()
+        {
+            return View();
+        }
+        public ActionResult productDetails()
+        {
+            return View();
+        }
+    }
+}
